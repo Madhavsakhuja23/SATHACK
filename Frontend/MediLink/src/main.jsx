535b6c5ea2a438efeccb64542c75e2cfdbe5e66f
geetika-kathpal836 <geetikakathpal77@gmail.com>
2025-11-05 20:28:47 +0000

    singup-loginn-added

--- Frontend/MediLink/src/main.jsx
+++ Frontend/MediLink/src/main.jsx
@@ -6,6 +6,8 @@ import { AuthProvider } from './components/ui/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <AuthProvider>
     <App />
+    </AuthProvider>
   </StrictMode>,
 )
